2cd815b1ef6e28d4db6f7c18b0477fb9c4caf4f7
sarvesh2000 <sarvesh4232@gmail.com>
2020-06-14 12:47:13 +0530

    Trying to print Message

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Hi")
+	bot.reply_to(message, message)
     
 
 @app.route('/' + TOKEN, methods=['POST'])
